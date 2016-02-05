efae9fcbc1660b744e62cc96ceb01a13f557769a
smikail <seyedmikail@gmail.com>
2016-02-05 10:05:19 -0800

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return 'بات '.. ورژن .. [[ 
+2ن ربات: 1.5
+ایدی سودوها:
+@Xx_king_salib_Xx
+@Mr_Vigeo
+@Xx_minister_salib_xX
+]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "ورژن: Shows bot version",
+  patterns = {
+    "^ورژن$"
+  }, 
+  run = run 
+}
+
+end
